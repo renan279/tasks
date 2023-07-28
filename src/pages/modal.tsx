a17a9c152529eed8217cf6a9eb7f114f6ae175db
Renan <renanfrancisco.on@gmail.com>
2023-07-28 04:33:16 -0300

    [ADD]const prisma

--- src/pages/modal.tsx
+++ src/pages/modal.tsx
@@ -6,6 +6,9 @@
 //     }
 //   })
 // }
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ datasources: {  db: { url: "mysql://3tyo9geeadovxfe3ps7w:pscale_pw_oWvwQy5Cc5eLgHYugVWVgBtx2onumEYBBI0jz7C708N@aws.connect.psdb.cloud/to-do?sslaccept=strict" } } });
 
 export default function Home() {
   return (
