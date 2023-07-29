61f4fc16d41b317b7a397bf22ec1bde1ef1dd6af
Renan <renanfrancisco.on@gmail.com>
2023-07-29 13:22:40 -0300

    POST ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  // content: [
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+  //   // Or if using `src` directory:
+  //   "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  // ],
   theme: {
     extend: {
       backgroundImage: {
