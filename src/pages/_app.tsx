9d535490bb6da832781a08f37ff5a584903a945e
Renan <renanfrancisco.on@gmail.com>
2023-07-31 17:11:58 -0300

    MyToast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// import ToastAnimated, { showToast } from "../components/MyToast";
+import ToastAnimated, { showToast } from "../components/MyToast";
 // import Notifications from '../pages/Notifications';
 import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // const handleClick = () =>
-  //   showToast({ type: "success", message: "Mensagem de sucesso" });
+  const handleClick = () =>
+    showToast({ type: "success", message: "Mensagem de sucesso" });
   return (
     <>
       <Component {...pageProps} />
